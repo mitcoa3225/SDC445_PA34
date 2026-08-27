3e0cce6d6a79ff5597fb15b33ba1e3e97ea3d00b
Mitchell Coates <mitcoa3225@students.ecpi.edu>
2026-08-27 12:43:33 -0400

    Add reset feature to counter

--- src/Counter.js
+++ src/Counter.js
@@ -12,14 +12,18 @@ function Counter() {
       setCount(count - 1);
     }
   };
-
+  const resetCount = () => {
+  setCount(0);
+  };
   return (
     <div>
       <h2>Task 1: Simple Counter</h2>
       <p>Current Count: {count}</p>
+      <button onClick={resetCount}>Reset</button>
 
       <button onClick={incrementCount}>Increment</button>
       <button onClick={decrementCount}>Decrement</button>
+      
     </div>
   );
 }
